9cfe66cd22ce46b8d195202582e0ca708796ebba
thejatmik <jatmikatejasukmana@gmail.com>
2020-04-21 11:48:55 +0700

    Detail Screen
    - fix landing reducer not saving user credential to state
    - detail screen toggling owner action button
    - detail screen toggling attendee action button

--- ktemuan/components/DetailScreen/DetailItem.js
+++ ktemuan/components/DetailScreen/DetailItem.js
@@ -3,14 +3,16 @@ import { Layout, Text, Divider, Button } from '@ui-kitten/components';
 import { ScrollView, TouchableHighlight, Image } from 'react-native';
 import { styles } from '../../styles';
 import { useSelector, useDispatch } from 'react-redux';
+import { apiURL } from '../../store/action/index';
 
 const placeholder = 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png';
 
 function DetailItem({ navigation }) {
   const dispatch = useDispatch()
   const event = useSelector(state => state.detail.event);
+  const userCred = useSelector(state => state.landing.userCred);
   
-  console.log(event);
+  // console.log(event);
   // console.log(event.image_url);
 
   let atendeeList= <></>
@@ -20,7 +22,7 @@ function DetailItem({ navigation }) {
         <Layout style={{elevation: 5, borderRadius: 20}}>
           <Avatar 
             size='medium' 
-            source={{ uri: `${ item.User.photo_url || `https://api.adorable.io/avatars/125/${item.User.email}.png` }` }}
+            source={{ uri: `${ apiURL + '/' + item.User.photo_url || `https://api.adorable.io/avatars/125/${item.User.email}.png` }` }}
           />
         </Layout>
       </TouchableHighlight>
@@ -30,12 +32,27 @@ function DetailItem({ navigation }) {
   let tagsList = <></>
   if (event.EventTags) {
     tagsList = event.EventTags.map(item => (
-      <Button size='tiny'>
+      <Button size='tiny' key={item.TagId} style={{backgroundColor: 'grey', borderColor: 'grey'}}>
         { item.Tag.name }
       </Button>
     ))
   }
+  let ownerActionStyle = 'none'
+  console.log(userCred, event.User)
+  if (event.User) {
+    console.log('is owner', Number(userCred.id) === Number(event.User.id))
+    console.log(userCred, event.User.id)
+    ownerActionStyle = Number(userCred.id) === Number(event.User.id) ? 'flex' : 'none';
+  } 
 
+  let attendedActionStyle = 'flex'
+  if (userCred.id && event.Attendees) {
+    event.Attendees.forEach(item => {
+      console.log(item)
+      if (item.User.id === userCred.id) attendedActionStyle = 'none'
+    })
+  }
+  
   return (
   <Layout style={{flex: 1, paddingTop: 5, paddingBottom: 0, paddingHorizontal: 5}}>
     <ScrollView>
@@ -69,11 +86,19 @@ function DetailItem({ navigation }) {
             style={{height: 100, width: 150, marginHorizontal: 1}}
           /> */}
         </ScrollView>
-        <Text category='h3'>Actions</Text>
-        <Layout>
-          <Button>Join</Button>
-          <Divider/>
-          <Button>unJoin ?</Button>
+        <Layout style={{display: attendedActionStyle}}>
+          <Text category='h5'>Non owner Action</Text>
+          <Layout style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+            <Button size='small'>Join Event</Button>
+            <Button size='small'>Remove from Join</Button>
+          </Layout>
+          <Layout style={{display: ownerActionStyle}}>
+            <Text category='h5'>Owner Action</Text>
+            <Layout style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+              <Button size='small'>Update Event</Button>
+              <Button size='small'>Delete Event</Button>
+            </Layout>
+          </Layout>
         </Layout>
         <TouchableHighlight onPressOut={() => navigation.navigate('Atendee', { eventId: event.id })}>
           <Layout style={{flexDirection: 'row', flexWrap: 'wrap', maxHeight: 100}}>
