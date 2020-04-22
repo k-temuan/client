df539b44ea411d9841efb5daee2dc4b4d99862c2
ryanadhi <ryanadhi.p@gmail.com>
2020-04-23 06:40:15 +0700

    loading

--- ktemuan/components/AttendeeListScreen/AttendeeListLoading.js
+++ ktemuan/components/AttendeeListScreen/AttendeeListLoading.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import { Layout, Text } from '@ui-kitten/components';
-import { styles } from '../../styles';
+import React from "react";
+import { Button, StyleSheet, View } from "react-native";
+import LottieView from "lottie-react-native";
 
-function AttendeeListLoading({ navigation }) {
-  return (
-    <Layout style={[styles.container]}>
-      <Text>Loading attendee list...</Text>
-    </Layout>
-  )
+export default class App extends React.Component {
+  componentDidMount() {
+    this.animation.play();
+    // Or set a specific startFrame and endFrame with:
+    // this.animation.play(30, 120);
+  }
+
+  resetAnimation = () => {
+    this.animation.play();
+  };
+
+  render() {
+    return (
+      <View style={styles.animationContainer}>
+        <LottieView
+          ref={(animation) => {
+            this.animation = animation;
+          }}
+          style={{
+            width: 400,
+            height: 400,
+          }}
+          source={require("../../animation.json")}
+          // OR find more Lottie files @ https://lottiefiles.com/featured
+          // Just click the one you like, place that file in the 'assets' folder to the left, and replace the above 'require' statement
+        />
+      </View>
+    );
+  }
 }
 
-export default AttendeeListLoading;
+const styles = StyleSheet.create({
+  animationContainer: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+});
